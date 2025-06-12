72d832c3ad2a18f4911089949fdba02ad6288ff1
FINALIO <ivanhrunchyk111@email.com>
2025-06-12 20:26:40 +0300

    rtyuiop

--- New_Flask/app.py
+++ New_Flask/app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index")
 @app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-if __name__ == ("__main__"):
-    app.run(debug = True)
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
+if __name__ == "__main__":
+    app.run(debug=True)
